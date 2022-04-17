fb43ccc0ec1282309eecb9091a9fb6249c27fa94
garman000 <garman000@gmail.com>
2022-04-17 16:52:15 +0300

    added style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Alert, Button } from "react-bootstrap";
+import { Alert, Button, Row, Col, Container } from "react-bootstrap";
+
 
 function HomePage() {
   return (
     <>
+    <Container>
       <Alert variant="info">
         Hi Anakin Skywalker, the last time you played you received a score of
         1610305 and your higher score is 101513213!!
@@ -12,12 +14,30 @@ function HomePage() {
         You can press the button (no button yet) to start a new game and you
         will be redirected to the /games page!
       </Alert>
-      <Button
-        variant="outline-primary"
-        type="button"
-      >
+      <Button variant="outline-primary" type="button">
         Start the Game!!!!
       </Button>
+  
+        <Row>
+      <Col xs lg="5 px-5" ><div className="imgTest header text rps"></div> </Col>
+      </Row>
+  
+      <Row>
+        {/* <div className="testt"> */}
+        <Col xs lg="5 px-5"><div className="imgTest header text tetris"></div> </Col>
+        {/* </div> */}
+        <Col xs lg="5"><div className="imgTest header text snake"></div></Col>
+      </Row>
+      <Row>
+        <Col><div className="imgTest header text pacman"></div></Col>
+        <Col><div className="imgTest header text spaceInvaders"></div></Col>
+        <Col><div className="imgTest header text mario"></div></Col>
+      </Row>
+    
+      
+      
+      
+      </Container>
     </>
   );
 }
