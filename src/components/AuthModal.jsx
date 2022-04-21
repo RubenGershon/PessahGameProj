311f6308c58989a3de813ad2902b5ab4e47fd1cb
Ruben Gershon <65229791+RubenGershon@users.noreply.github.com>
2022-04-21 13:21:46 +0300

    fixes

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -5,7 +5,9 @@ import { Modal } from "react-bootstrap";
 function AuthModal({ show, onClose }) {
   return (
     <Modal show={show} onHide={onClose}>
-      <Modal.Header closeButton></Modal.Header>
+      <Modal.Header closeButton>
+        <Modal.Title>Create a new account!!</Modal.Title>
+      </Modal.Header>
       <Modal.Body>
         <SignUp closeModal={onClose} />
       </Modal.Body>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -15,6 +15,21 @@ function SignUp({ closeModal }) {
   const navigate = useNavigate();
 
   async function onSignUpWrapper() {
+    if (
+      !firstName ||
+      !lastName ||
+      !nickName ||
+      !email ||
+      !password ||
+      !confirmPassword
+    ) {
+      setSignUpErr("Missing information - all fields must be filled.");
+      return;
+    }
+    if (password != confirmPassword) {
+      setSignUpErr("Passwords don't match");
+      return;
+    }
     try {
       const response = await onSignUp({
         first_name: firstName,
@@ -34,78 +49,69 @@ function SignUp({ closeModal }) {
   }
 
   return (
-    <>
-      <h5 className="display-5">Create a new acount!</h5>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>NickName</Form.Label>
-          <Form.Control
-            type="name"
-            placeholder="Enter a NickName"
-            value={nickName}
-            onChange={(e) => setnickName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type="name"
-            placeholder="Enter first name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="name"
-            placeholder="Enter last name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPwd(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password Confirmation</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Re-type your Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </Form.Group>
-        {signUpErr && (
-          <Alert variant="danger">{signUpErr}</Alert>
-        )}
-        <Button
-          variant="outline-primary"
-          type="button"
-          onClick={onSignUpWrapper}
-        >
-          Create Account
-        </Button>
-      </Form>
-    </>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>NickName</Form.Label>
+        <Form.Control
+          type="name"
+          placeholder="Enter a NickName"
+          value={nickName}
+          onChange={(e) => setnickName(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control
+          type="name"
+          placeholder="Enter first name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control
+          type="name"
+          placeholder="Enter last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPwd(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password Confirmation</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Re-type your Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </Form.Group>
+      {signUpErr && <Alert variant="danger">{signUpErr}</Alert>}
+      <Button variant="outline-primary" type="button" onClick={onSignUpWrapper}>
+        Create Account
+      </Button>
+    </Form>
   );
 }
 
