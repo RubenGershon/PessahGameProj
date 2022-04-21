29c27fa49a5186047ccb532bf49e1fe63f997bce
garman000 <garman000@gmail.com>
2022-04-21 12:04:29 +0300

    created GET user nickname FE

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../scss/game.scss";
 import "../scss/header.scss";
 import Header from "./Header";
+// import { useAuth } from "./AuthProvider" 
 // import { Alert } from "react-bootstrap";
 
 
-const Game = ({ score, setScore, myChoice }) => {
+const Game = ({ score, setScore, myChoice, }) => {
+  // const {activeUser} = useAuth();
   const [house, setHouse] = useState("");
   const [playerWin, setPlayerWin] = useState("");
-
   const [counter, setCounter] = useState(3);
+  const [attempt, setAttempt] = useState(null)
+  const [attemptButton, sendAttemptButton] = useState(false)
+  const [sendScore, setSendScore] = useState()
+
 
   const newHousePick = () => {
     const choices = ["rock", "paper", "scissors"];
@@ -26,24 +31,35 @@ const Game = ({ score, setScore, myChoice }) => {
     if (myChoice === "rock" && house === "scissors") {
       setPlayerWin("win");
       setScore(score + 5);
+      setAttempt( attempt + 1)
     } else if (myChoice === "rock" && house === "paper") {
       setPlayerWin("lose");
       setScore(score - 1);
+      setAttempt( attempt + 1)
+
     } else if (myChoice === "scissors" && house === "paper") {
       setPlayerWin("win");
       setScore(score + 5);
+      setAttempt( attempt + 1)
+
     } else if (myChoice === "scissors" && house === "rock") {
       setPlayerWin("lose");
       setScore(score - 1);
+      setAttempt( attempt + 1)
+
     } else if (myChoice === "paper" && house === "rock") {
       setPlayerWin("win");
       setScore(score + 5);
+      setAttempt( attempt + 1)
+
     } else if (myChoice === "paper" && house === "scissors") {
       setPlayerWin("lose");
       setScore(score - 1);
+      setAttempt( attempt + 1)
+
     } else {
       setPlayerWin("draw");
-    }
+    } setAttempt(0)
   };
 
   useEffect(() => {
@@ -58,6 +74,28 @@ const Game = ({ score, setScore, myChoice }) => {
       clearInterval(timer);
     };
   }, [counter, house]);
+
+  // useEffect(() => {
+  //   if (attempts === 5 ) {
+  //     sendAttemptButton(true)
+  //   }
+  // })
+
+  // function handleScore(score) {
+  //   let newScore = {
+  //     email: activeUser.email,
+  //     score: score
+  //   } 
+  //   axios.post("http://localhost:8080/add", newScore)
+  //     .then((res) => {
+  //       if(res) {
+  //         setRecentScore(score);
+  //         getHighScore(activeUser.email);
+  //         alert("Score sent")
+  //       }
+  //     })
+    
+  // }
 
   return (
       <Container>
@@ -105,6 +143,7 @@ const Game = ({ score, setScore, myChoice }) => {
           <div className="counter">{counter}</div>
         )}
       </div>
+      <Button>Send High Score</Button>
 
    
     </div>
